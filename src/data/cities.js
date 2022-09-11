50120f61a43cf89812a97f889375b1813c63558d
Marcos Baravalle <marcosabaravalle@gmail.com>
2022-09-10 21:17:56 -0300

    first commit

---
+++ src/data/cities.js
@@ -0,0 +1,23 @@
+const cities = [
+  {
+    id: 0,
+    name: "London",
+  },
+  {
+    id: 1,
+    name: "Sydney",
+  },
+  {
+    id: 2,
+    name: "Tokyo",
+  },
+  {
+    id: 3,
+    name: "Toronto",
+  },
+  {
+    id: 4,
+    name: "Paris",
+  },
+];
+export default cities;
